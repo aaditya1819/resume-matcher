a94ccfdc1be033dc9a8bec4f3444684bb5a1f779
aaditya1819 <aadityadhanwate830@gmail.com>
2025-08-14 10:53:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -120,4 +120,6 @@ def matcher():
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Get port from environment
+    app.run(host='0.0.0.0', port=port, debug=False)
+
